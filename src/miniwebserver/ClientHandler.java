21605b42f50acc135985d8c4a295b7feda602c2a
unknown <ijeddleblute@mix.wvu.edu>
2018-03-06 17:49:32 -0500

    String Changed

--- src/miniwebserver/ClientHandler.java
+++ src/miniwebserver/ClientHandler.java
@@ -19,8 +19,8 @@ public class ClientHandler extends Thread {
     PrintWriter outWriter;
     BufferedReader inReader;
     Socket socket;
-    String strMessage = "Click Here<br>\n" +
-        "<button onclick='alert(\"Someone is a gullibull.\")'>CLICK</button>";
+    String strMessage = "Click Here for Isaac's name<br>\n" +
+        "<button onclick='alert(\"Isaac Eddleblute\")'>CLICK</button>";
     
     public ClientHandler(Socket sock){
         try{
