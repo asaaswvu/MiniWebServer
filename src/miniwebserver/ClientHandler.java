b8303b6451a436fc20a0ce1f32996ed035f2db70
Joseph Smith <jdsmith4@mix.wvu.edu>
2018-03-06 20:35:22 -0500

    Changes made to strMessage

--- src/miniwebserver/ClientHandler.java
+++ src/miniwebserver/ClientHandler.java
@@ -19,8 +19,8 @@ public class ClientHandler extends Thread {
     PrintWriter outWriter;
     BufferedReader inReader;
     Socket socket;
-    String strMessage = "Click Here<br>\n" +
-        "<button onclick='alert(\"Someone is a gullibull.\")'>CLICK</button>";
+    String strMessage = "Don't Click Here<br>\n" +
+        "<button onclick='alert(\"Joseph Smith.\")'>CLICK</button>";
     
     public ClientHandler(Socket sock){
         try{
