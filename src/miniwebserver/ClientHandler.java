65b0334ac1675f46e91fb9525224bf952b2e0062
Abdullah M A M A Alnaibari <amalnaibari@ubuntu.statler.wvu.edu>
2018-03-08 15:13:28 -0500

    i've done this on 3:13

--- src/miniwebserver/ClientHandler.java
+++ src/miniwebserver/ClientHandler.java
@@ -19,8 +19,8 @@ public class ClientHandler extends Thread {
     PrintWriter outWriter;
     BufferedReader inReader;
     Socket socket;
-    String strMessage = "Click Here<br>\n" +
-        "<button onclick='alert(\"Someone is a gullibull.\")'>CLICK</button>";
+    String strMessage = "Please Click Here<br>\n" +
+        "<button onclick='alert(\"Hello this is abdullah alnaibari.\")'>CLICK</button>";
     
     public ClientHandler(Socket sock){
         try{
