c429edaafd9919311a58beb68ba20694845af84a
Brandon Quan <btq0002@mix.wvu.edu>
2018-03-09 13:07:49 -0500

    lab 6

--- src/miniwebserver/ClientHandler.java
+++ src/miniwebserver/ClientHandler.java
@@ -20,7 +20,7 @@ public class ClientHandler extends Thread {
     BufferedReader inReader;
     Socket socket;
     String strMessage = "Click Here<br>\n" +
-        "<button onclick='alert(\"Brandon Quan.\")'>CLICK</button>";
+        "<button onclick='alert(\"Christine Frying.\")'>CLICK</button>";
     
     public ClientHandler(Socket sock){
         try{
